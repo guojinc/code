cfe01c36d1c4394147821c22f29031db881b01ba
dascard <dascard601@gamil.com>
2024-09-11 23:09:33 +0800

    test

--- java/MyClassTest.java
+++ java/MyClassTest.java
@@ -9,9 +9,15 @@ import static org.junit.Assert.*;
 
 public class MyClassTest {
     @Test
-    public void testAdd() {
+    public void testAdd1() {
         MyClass myClass = new MyClass();
         assertEquals(5, myClass.add(2, 3));
     }
+	@Test
+	public void testAdd2()
+	{
+		MyClass myClass = new MyClass();
+		assertEquals(3, myClass.add(1, 2));
+	}
 }
 
