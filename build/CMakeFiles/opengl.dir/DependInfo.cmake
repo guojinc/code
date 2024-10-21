d79697342a27a3e00cc83be0e53fc4cabb8f9436
dascard <143863211+dascard@users.noreply.github.com>
2024-10-21 02:48:20 +0000

    devcontainer

---
+++ build/CMakeFiles/opengl.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/workspaces/code/opengl/src/glad.c" "/workspaces/code/build/CMakeFiles/opengl.dir/src/glad.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/workspaces/code/opengl/include"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/workspaces/code/opengl/src/main.cpp" "/workspaces/code/build/CMakeFiles/opengl.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/workspaces/code/opengl/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
